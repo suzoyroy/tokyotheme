ee45c278416f0e3218d51493d92537f462b7cfc5
SUJOY ROY <88615084+suzoyroy@users.noreply.github.com>
2022-01-07 00:00:25 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+    var typed = new Typed('#text', {
+        strings: ['WEB DEVELOPER.','WEB DESIGNER.','FREELANCER.'],
+        smartBackspace: true,
+        typeSpeed: 70,
+        backSpeed: 70,
+        loop: true,
+        showCursor: true,
+        cursorChar: '|',
+        backDelay: 1500,
+
+      });
+      
+
+      
